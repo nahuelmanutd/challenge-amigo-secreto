c839c3bfb12118fc7bb2170e1ced5ea77dd958ad
nahuelmanutd <161343338+nahuelmanutd@users.noreply.github.com>
2025-08-12 19:46:39 -0300

    Versión terminada de app.js

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayNombres = [];
+
+function asignarTextoElemento(elementoId,texto) {
+    let elementoHTML = document.getElementById(elementoId); //tiene que tener id escrita con comillas
+
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function borrarTexto(){
+    return document.getElementById("amigo").value = "";
+}
+
+function agregarAmigo() {
+    let nombreElegido = document.getElementById("amigo").value; //botón añadir
+
+    if(nombreElegido == "") {
+        alert("Ingrese un nombre válido"); //si no se introduce texto
+    } else {
+        arrayNombres.push(nombreElegido); //agrega el nombre elegido al array
+        mostrarListaAmigos();
+    }
+    borrarTexto();
+}
+
+function mostrarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = ""; //limpia la lista antes de agregar
+    for(i = 0; i < arrayNombres.length; i++) {
+        let li = document.createElement("li"); //crea el elemento <li> en HTML
+        li.textContent = arrayNombres[i]; //el contenido de la lista actual va a cambiar a medida que i vaya aumentando
+        lista.appendChild(li); //esto asocia <ul> a <li>
+    }
+}
+
+function sortearAmigo() {
+    if (arrayNombres.length === 0) { //verifica que el array no esté vacío
+        alert("No hay nombres para sortear.");
+        return;
+    }
+
+    let generadorDeIndiceAleatorio = Math.floor(Math.random()*arrayNombres.length);
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = arrayNombres[generadorDeIndiceAleatorio]; //sortea el número
+}
