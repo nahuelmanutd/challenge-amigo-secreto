970c1fd7f3494dfe494eb60590c414f50da38718
nahuelmanutd <161343338+nahuelmanutd@users.noreply.github.com>
2025-08-12 19:59:58 -0300

    Comentarios de app.js
    Le agrega comentarios para mostrar el contexto de cada función.

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
+//Crea la lista de los nombres comenzando vacía.
 let arrayNombres = [];
 
-function asignarTextoElemento(elementoId,texto) {
-    let elementoHTML = document.getElementById(elementoId); //tiene que tener id escrita con comillas
-
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
+//Borra el nombre luego de ingresarlo.
 function borrarTexto(){
     return document.getElementById("amigo").value = "";
 }
 
+//Agrega el nombre ingresado a la lista, en caso de que no haya nada escrito, se muestra una alerta.
 function agregarAmigo() {
     let nombreElegido = document.getElementById("amigo").value; //botón añadir
 
     if(nombreElegido == "") {
-        alert("Ingrese un nombre válido"); //si no se introduce texto
+        alert("Ingrese un nombre válido");
     } else {
-        arrayNombres.push(nombreElegido); //agrega el nombre elegido al array
+        arrayNombres.push(nombreElegido);
         mostrarListaAmigos();
     }
     borrarTexto();
 }
 
+//Agrega los nombres en la lista bajo la caja de texto.
 function mostrarListaAmigos() {
     let lista = document.getElementById("listaAmigos");
 
-    lista.innerHTML = ""; //limpia la lista antes de agregar
+    lista.innerHTML = "";
     for(i = 0; i < arrayNombres.length; i++) {
-        let li = document.createElement("li"); //crea el elemento <li> en HTML
-        li.textContent = arrayNombres[i]; //el contenido de la lista actual va a cambiar a medida que i vaya aumentando
-        lista.appendChild(li); //esto asocia <ul> a <li>
+        let li = document.createElement("li");
+        li.textContent = arrayNombres[i];
+        lista.appendChild(li);
     }
 }
 
+//Sortea el número aleatorio y, en caso de que no haya ninguno escrito, muestra una alerta.
 function sortearAmigo() {
     if (arrayNombres.length === 0) { //verifica que el array no esté vacío
         alert("No hay nombres para sortear.");
@@ -43,5 +41,6 @@ function sortearAmigo() {
     let generadorDeIndiceAleatorio = Math.floor(Math.random()*arrayNombres.length);
     let resultado = document.getElementById("resultado");
 
-    resultado.innerHTML = arrayNombres[generadorDeIndiceAleatorio]; //sortea el número
+    resultado.innerHTML = arrayNombres[generadorDeIndiceAleatorio];
 }
+
